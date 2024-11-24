809bed705ee81597d09831fdf84ad761cf016566
Daniel Androse P <danielandrose.p2022ece@sece.ac.in>
2024-11-24 19:17:02 +0530

    index

--- index.js
+++ index.js
@@ -18,8 +18,6 @@ app.get("/", (req, res) => {
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Welcome Page</title>
             <style>
                 body {
